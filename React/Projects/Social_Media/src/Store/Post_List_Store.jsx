0807de30a84bb90f4785b046a0451cd08912f903
amanmishra <amanmishra15.08.2005@gmail.com>
2026-09-15 02:02:21 +0530

    Learning more concepts of React

---
+++ React/Projects/Social_Media/src/Store/Post_List_Store.jsx
@@ -0,0 +1,91 @@
+import { createContext, useReducer } from "react";
+
+export const PostList = createContext({
+  postList: [],
+  addPost: () => {},
+  addInitialPosts: () => {},
+  deletePost: () => {},
+});
+
+const postListReducer = (currPostList, action) => {
+  let newPostList = currPostList;
+  if (action.type === "DELETE_POST") {
+    newPostList = currPostList.filter(
+      (post) => post.id !== action.payloads.postId,
+    );
+  } else if (action.type === "ADD_INITIAL_POSTS") {
+    newPostList = action.payloads.posts;
+  } else if (action.type === "ADD_POST") {
+    newPostList = [action.payloads, ...currPostList];
+  }
+  return newPostList;
+};
+
+const PostListProvider = ({ children }) => {
+  const [postList, dispatchPostList] = useReducer(
+    postListReducer,
+    [],
+    // DEFAULT_POST_LIST,
+  );
+
+  const addPost = (userId, postTitle, postBody, reactions, tags) => {
+    dispatchPostList({
+      type: "ADD_POST",
+      payloads: {
+        id: Date.now(),
+        title: postTitle,
+        body: postBody,
+        reactions: reactions,
+        userId: userId,
+        tags: tags,
+      },
+    });
+  };
+
+  const addInitialPosts = (posts) => {
+    dispatchPostList({
+      type: "ADD_INITIAL_POSTS",
+      payloads: {
+        posts,
+      },
+    });
+  };
+
+  const deletePost = (postId) => {
+    dispatchPostList({
+      type: "DELETE_POST",
+      payloads: {
+        postId,
+      },
+    });
+  };
+
+  return (
+    <PostList.Provider
+      value={{ postList, addPost, addInitialPosts, deletePost }}
+    >
+      {children}
+    </PostList.Provider>
+  );
+};
+
+// const DEFAULT_POST_LIST = [
+//   {
+//     id: 1,
+//     title: "Going to Mumbai",
+//     body: "Hii Friends, I am going to Mumbai for my Vacations. Hope to enjoy a lot. Peace Out.",
+//     reactions: 2,
+//     userId: "user-9",
+//     tags: ["Vacations", "Mumbai", "Enjoying"],
+//   },
+//   {
+//     id: 2,
+//     title: "Pass ho gye Bhai",
+//     body: "4 saal k masti k baad bhi ho gye pass. Hard to Beleive",
+//     reactions: 15,
+//     userId: "user-2",
+//     tags: ["Graduating", "Unbelievable"],
+//   },
+// ];
+
+export default PostListProvider;
